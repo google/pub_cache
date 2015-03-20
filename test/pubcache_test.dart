7a5f8cbeaaf2409035422f91f1f28e65d723ec3d
Devon Carew <devoncarew@gmail.com>
2015-03-20 11:00:21 -0700

    update licenses

--- test/pubcache_test.dart
+++ test/pubcache_test.dart
@@ -1,5 +1,6 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
 
 library pubcache_test;
 
