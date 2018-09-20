50ce8a0894246e98c23878fd6aa29eb3db753e51
Joe Conway <joeconway3@gmail.com>
2018-09-20 16:06:19 -0400

    split test package activating into another script

---
+++ tool/travis-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Fast fail the script on failures.
+set -e
+
+# Activate some packages for use while running tests.
+pub global activate tuneup
+pub global activate path 1.6.2
+pub global activate path
+pub global activate --source git https://github.com/dart-lang/test.git
+pub global activate --source path .
