399dd52cdcdf79703aa0260ada73f4a694310201
Joe Conway <joeconway3@gmail.com>
2018-09-20 16:13:52 -0400

    add set -e back to primary script - oops

--- tool/travis.sh
+++ tool/travis.sh
@@ -4,6 +4,8 @@
 # for details. All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
 
+set -e
+
 # Verify that the libraries are error free.
 dartanalyzer --fatal-warnings .
 
